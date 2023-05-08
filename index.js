b87027e9d227601b3b981609a4253a74ace4019d
cristian <casberry41@gmail.com>
2023-05-08 17:00:12 -0500

    added license to js file

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-// List of licenses and badges, CREDIT github user lukas-h for badges
 const licenses = ['Apache License 2.0', 'GNU General Public License v3.0', 'MIT License', 'BSD 2-Clause "Simplified" License', 'Boost Software License 1.0', 'Creative Commons Zero v1.0 Universal'];
+
 const badges = [
     {license: 'Apache License 2.0', badge: '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'},
     {license: 'GNU General Public License v3.0', badge: '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)'},
@@ -70,7 +70,6 @@ function writeToFile(fileName, data) {
         }
     }
     console.log(yourBadge);
-
     // Write the README
     var content = 
 `# ${data.title}
@@ -110,5 +109,4 @@ function init() {
             writeToFile("customREADME.md", data);
         })
 }
-
 init();
